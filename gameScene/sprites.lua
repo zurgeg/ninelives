2db884fbee20bc803fc25ef63e9e20e3cb2ef20f
zurgeg <46549042+zurgeg@users.noreply.github.com>
2022-05-25 12:19:27 -0400

    Spread code out

---
+++ gameScene/sprites.lua
@@ -0,0 +1,39 @@
+import "CoreLibs/graphics"
+import "CoreLibs/sprites"
+import "AnimatedSprite/AnimatedSprite"
+
+local gfx <const> = playdate.graphics
+
+local catSprite = nil
+
+local hasUpdated = false
+
+function setUpSprites()
+    local catAnim = gfx.imagetable.new("gfx/anim/cat")
+    catSprite = AnimatedSprite.new(catAnim)
+    assert (catSprite, "Failed to create cat sprite")
+    catSprite:moveTo(200, 100)
+    catSprite:add()
+    catSprite:playAnimation()
+end
+
+function updateSprites()
+    if not hasUpdated then
+        hasUpdated = true
+        playdate.graphics.sprite.update()
+    end
+    if playdate.buttonJustPressed(playdate.kButtonA) or playdate.buttonJustPressed(playdate.kButtonB) or playdate.buttonJustPressed(playdate.kButtonUp) then
+        -- jump
+    end
+    if playdate.buttonIsPressed(playdate.kButtonLeft) then
+        catSprite.globalFlip = gfx.kImageFlippedX
+        catSprite:moveBy(-7, 0)
+        playdate.graphics.sprite.update()
+    end
+    if playdate.buttonIsPressed(playdate.kButtonRight) then
+        catSprite.globalFlip = gfx.kImageUnflipped
+        catSprite:moveBy(7, 0)
+        playdate.graphics.sprite.update()
+    end
+end
+
--- main.lua
+++ main.lua
@@ -1,44 +1,13 @@
-import "CoreLibs/object"
-import "CoreLibs/graphics"
-import "CoreLibs/sprites"
 import "CoreLibs/timer"
-import "CoreLibs/animation"
-import "AnimatedSprite/AnimatedSprite"
-
-local gfx <const> = playdate.graphics
-
-local catSprite = nil
-
-local hasUpdated = false
+import "gameScene/sprites.lua"
 
 function setUp()
-    local catAnim = gfx.imagetable.new("gfx/anim/cat")
-    catSprite = AnimatedSprite.new(catAnim)
-    assert (catSprite, "Failed to create cat sprite")
-    catSprite:moveTo(200, 100)
-    catSprite:add()
-    catSprite:playAnimation()
+    setUpSprites()
 end
 
 setUp()
 
 function playdate.update()
-    if not hasUpdated then
-        hasUpdated = true
-        playdate.graphics.sprite.update()
-    end
-    if playdate.buttonJustPressed(playdate.kButtonA) or playdate.buttonJustPressed(playdate.kButtonB) or playdate.buttonJustPressed(playdate.kButtonUp) then
-        -- jump
-    end
-    if playdate.buttonIsPressed(playdate.kButtonLeft) then
-        catSprite.globalFlip = gfx.kImageFlippedX
-        catSprite:moveBy(-7, 0)
-        playdate.graphics.sprite.update()
-    end
-    if playdate.buttonIsPressed(playdate.kButtonRight) then
-        catSprite.globalFlip = gfx.kImageUnflipped
-        catSprite:moveBy(7, 0)
-        playdate.graphics.sprite.update()
-    end
+    updateSprites()
     playdate.timer.updateTimers()
 end
