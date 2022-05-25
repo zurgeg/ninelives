2c111624b953604c4b5fd5c0c781a70d02924b64
zurgeg <46549042+zurgeg@users.noreply.github.com>
2022-05-25 12:12:50 -0400

    Use better animation

--- main.lua
+++ main.lua
@@ -3,37 +3,42 @@ import "CoreLibs/graphics"
 import "CoreLibs/sprites"
 import "CoreLibs/timer"
 import "CoreLibs/animation"
+import "AnimatedSprite/AnimatedSprite"
 
 local gfx <const> = playdate.graphics
 
 local catSprite = nil
 
-animX = 200
-animY = 120
+local hasUpdated = false
 
 function setUp()
-    catAnim = gfx.imagetable.new("gfx/anim/cat")
-    catAnimation = playdate.graphics.animation.loop.new(80, catAnim)
+    local catAnim = gfx.imagetable.new("gfx/anim/cat")
+    catSprite = AnimatedSprite.new(catAnim)
+    assert (catSprite, "Failed to create cat sprite")
+    catSprite:moveTo(200, 100)
+    catSprite:add()
+    catSprite:playAnimation()
 end
 
 setUp()
 
 function playdate.update()
-    gfx.clear()
-    animY = 120
+    if not hasUpdated then
+        hasUpdated = true
+        playdate.graphics.sprite.update()
+    end
     if playdate.buttonJustPressed(playdate.kButtonA) or playdate.buttonJustPressed(playdate.kButtonB) or playdate.buttonJustPressed(playdate.kButtonUp) then
         -- jump
     end
     if playdate.buttonIsPressed(playdate.kButtonLeft) then
-        -- move left
-        catAnimation.paused = false
-        animX -= 5
+        catSprite.globalFlip = gfx.kImageFlippedX
+        catSprite:moveBy(-7, 0)
+        playdate.graphics.sprite.update()
     end
     if playdate.buttonIsPressed(playdate.kButtonRight) then
-        -- move right
-        catAnimation.paused = false
-        animX += 5
+        catSprite.globalFlip = gfx.kImageUnflipped
+        catSprite:moveBy(7, 0)
+        playdate.graphics.sprite.update()
     end
-    catAnimation:draw(animX, animY)
     playdate.timer.updateTimers()
 end
