55a3c22186f56f354ddcb9f71e6b7e8893bf54c2
zurgeg <46549042+zurgeg@users.noreply.github.com>
2022-05-30 09:53:32 -0400

    add an assertion to check if I broke the metadata

--- main.lua
+++ main.lua
@@ -14,6 +14,7 @@ function SetScene(sceneID)
 end
 
 function SetUp()
+    assert(playdate.metadata.bundleID == "com.zurgeg.NineLives", "metadata is broken somehow")
     SetupRNG()
     SetUpLevelGen(0, 200, 20, 20, 0, 0)
     SetUpSprites()
