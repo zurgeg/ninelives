7d5ec91b28a3f52fee5faa42c7a4711f2919d3c1
zurgeg <46549042+zurgeg@users.noreply.github.com>
2022-05-29 23:46:46 -0400

    set up levelgen

--- main.lua
+++ main.lua
@@ -1,9 +1,11 @@
 import "CoreLibs/timer"
 import "gameScene/sprites.lua"
 import "rng/rng.lua"
+import "levelGeneration/levelGen.lua"
 
 function SetUp()
     SetupRNG()
+    SetUpLevelGen(0, 20, 100, 100, 0, DifficultyEasy)
     SetUpSprites()
 end
 
