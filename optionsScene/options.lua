eeec9e9772b10326fd30aedb45db6124c842dbff
zurgeg <46549042+zurgeg@users.noreply.github.com>
2023-11-04 15:04:11 -0400

    Commit from my old laptop a while ago...

--- optionsScene/options.lua
+++ optionsScene/options.lua
@@ -5,7 +5,7 @@ local gfx <const> = playdate.graphics
 function UpdateOptions()
     gfx.clear()
     gfx.drawText("No options available. Press Ⓑ to go back.", 0, 0, nil, kTextAlignment.center)
-    if playdate.buttonJustPressed(playdate.kButtonB) then
+    if playdate.buttonJustPressed("b") then
         SetScene(SceneMenu)
     end
 end
